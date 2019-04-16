926d9622dfaa9e4889ce976d25df06f01572e577
YoCiyy <289617084@qq.com>
2019-04-16 10:52:07 +0800

    新增mybatis增删除改操作测试实例，补充注释

--- fame-springboot-mybatis/src/main/java/com/fame/controller/UserController.java
+++ fame-springboot-mybatis/src/main/java/com/fame/controller/UserController.java
@@ -3,13 +3,11 @@ package com.fame.controller;
 import com.fame.entity.User;
 import com.fame.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
- *
  * @author Y.yang
  * @date 2019/4/14
  */
@@ -17,15 +15,52 @@ import java.util.List;
 @RestController
 public class UserController {
 
+	@Autowired
+	private UserMapper userMapper;
 
-    @Autowired
-    private UserMapper userMapper;
+	/**
+	 * 查询用户列表
+	 */
+	@GetMapping("/list")
+	public List<User> list() {
+		List<User> userList = userMapper.findAll();
+		System.out.println(userList);
+		return userList;
+	}
 
-    @RequestMapping("/list")
-    public void list(){
-        List<User> userList = userMapper.findAll();
-        System.out.println(userList);
-    }
+	/**
+	 * 通过ID获取用户信息
+	 */
+	@GetMapping("/get")
+	public User get() {
+		return userMapper.get(8L);
+	}
+
+	/**
+	 * 新增用户信息
+	 */
+	@PostMapping("/add")
+	public void add() {
+		User user = new User().setUsername("大白一号").setPassword("dabaiyihao").setAge(22);
+		userMapper.add(user);
+	}
+
+	/**
+	 * 修改用户信息
+	 */
+	@PutMapping("/update")
+	public void update() {
+		User user = userMapper.get(1L);
+		user.setUsername("大白修改操作");
+		userMapper.update(user);
+	}
+
+    /**
+     * 删除用户信息
+     */
+    @DeleteMapping("/delete")
+	public void delete() {
+	    userMapper.delete(9L);
+	}
 
 }
-
