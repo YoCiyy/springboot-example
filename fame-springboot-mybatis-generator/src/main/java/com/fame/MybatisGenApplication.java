32d0ea437b9673591b3dd8a33031eb2ecd5ec234
YoCiyy <289617084@qq.com>
2019-04-17 10:41:52 +0800

    新增mybatis-generator逆向工程模块

---
+++ fame-springboot-mybatis-generator/src/main/java/com/fame/MybatisGenApplication.java
@@ -0,0 +1,17 @@
+package com.fame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Mybatis-generator启动类
+ * 
+ * @author Y.yang
+ * @date 2019/4/16
+ */
+@SpringBootApplication
+public class MybatisGenApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisGenApplication.class, args);
+	}
+}
---
+++ fame-springboot-mybatis-generator/src/main/java/com/fame/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.fame.controller;
+
+/**
+ *
+ * @author Y.yang
+ * @date 2019/4/16
+ */
+public class UserController {
+}
