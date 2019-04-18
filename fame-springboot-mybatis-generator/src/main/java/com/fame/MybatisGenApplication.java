342c50237abe978950afb9b7a616645f13cb5233
YoCiyy <289617084@qq.com>
2019-04-18 11:25:34 +0800

    测试代码调整

--- fame-springboot-mybatis-generator/src/main/java/com/fame/MybatisGenApplication.java
+++ fame-springboot-mybatis-generator/src/main/java/com/fame/MybatisGenApplication.java
@@ -1,5 +1,6 @@
 package com.fame;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/4/16
  */
 @SpringBootApplication
+// mapper 接口类扫描包配置
+@MapperScan("com.fame.mapper")
 public class MybatisGenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MybatisGenApplication.class, args);
