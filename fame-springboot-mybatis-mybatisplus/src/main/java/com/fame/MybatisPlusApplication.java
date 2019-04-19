43a6c6891df507032783aa9775866fcf314afd24
YoCiyy <289617084@qq.com>
2019-04-19 08:46:30 +0800

    新增springboot-mybatisplus相关依赖

---
+++ fame-springboot-mybatis-mybatisplus/src/main/java/com/fame/MybatisPlusApplication.java
@@ -0,0 +1,8 @@
+package com.fame;
+
+/**
+ * Created by Y.yang on 2019/4/18.
+ */
+public class MybatisPlusApplication {
+
+}
---
+++ fame-springboot-mybatis-mybatisplus/src/main/java/com/fame/controller.java
@@ -0,0 +1,7 @@
+package com.fame;
+
+/**
+ * Created by Y.yang on 2019/4/18.
+ */
+public class controller {
+}
