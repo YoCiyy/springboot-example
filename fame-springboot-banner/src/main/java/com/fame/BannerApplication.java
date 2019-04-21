9745091de0080a1c3f1a715316a19d96aab27cc4
YoCiyy <289617084@qq.com>
2019-04-21 22:36:32 +0800

    新增springboot小功能--banner图修改

---
+++ fame-springboot-banner/src/main/java/com/fame/BannerApplication.java
@@ -0,0 +1,18 @@
+package com.fame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * banner图启动类
+ * 
+ * @author Y.yang
+ * @date 2019/4/21
+ */
+@SpringBootApplication
+public class BannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerApplication.class, args);
+	}
+}
