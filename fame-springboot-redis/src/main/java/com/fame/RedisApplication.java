6535852679fbb9f9c0c6c7b13f265ce201960c56
YoCiyy <289617084@qq.com>
2019-04-26 14:53:07 +0800

    新增springboot-redis整合模块

---
+++ fame-springboot-redis/src/main/java/com/fame/RedisApplication.java
@@ -0,0 +1,18 @@
+package com.fame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot-redis 整合启动类
+ * 
+ * @author Y.yang
+ * @date 2019/4/26
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+}
