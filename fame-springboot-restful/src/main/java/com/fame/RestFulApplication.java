89dfc0c7f0ea35a468df3bcebcd3489c3e5d30ef
YoCiyy <289617084@qq.com>
2019-04-30 15:47:29 +0800

    新增restful风格模块整合

---
+++ fame-springboot-restful/src/main/java/com/fame/RestFulApplication.java
@@ -0,0 +1,17 @@
+package com.fame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * WEB Restful架构风格启动类
+ * 
+ * @author Y.yang
+ * @date 2019/4/30
+ */
+@SpringBootApplication
+public class RestFulApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulApplication.class, args);
+	}
+}
